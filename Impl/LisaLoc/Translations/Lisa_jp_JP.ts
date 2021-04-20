4d6a2d321b3aee017fdcf42eb836955bdcdc48dd
vc <vc@21205d9f-8918-2240-a3d5-c39064a2c852>
2021-04-20 13:54:47 +0000

    Localization
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@3145 21205d9f-8918-2240-a3d5-c39064a2c852

--- Impl/LisaLoc/Translations/Lisa_jp_JP.ts
+++ Impl/LisaLoc/Translations/Lisa_jp_JP.ts
@@ -43,7 +43,7 @@
     <message>
         <location line="+6"/>
         <location line="+6"/>
-        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="-74"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="-76"/>
         <location line="+4"/>
         <source>Account info</source>
         <translation type="unfinished"></translation>
@@ -51,21 +51,21 @@
     <message>
         <location line="-8"/>
         <location line="+40"/>
-        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="-6"/>
-        <location line="+40"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="-2"/>
+        <location line="+38"/>
         <source>Accounts</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="-34"/>
-        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="-32"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="-34"/>
         <source>AccountInfo</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="-2"/>
         <location line="+10"/>
-        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="-4"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="+2"/>
         <location line="+10"/>
         <source>Contact Files</source>
         <translation type="unfinished"></translation>
@@ -73,20 +73,20 @@
     <message>
         <location line="-2"/>
         <location line="+6"/>
-        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="-2"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="-8"/>
         <location line="+4"/>
         <source>Contact info</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="-8"/>
-        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="-6"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="-2"/>
         <source>Contacts</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="+6"/>
-        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="+8"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="+4"/>
         <source>ContactInfo</source>
         <translation type="unfinished"></translation>
     </message>
@@ -101,8 +101,8 @@
     <message>
         <location line="-10"/>
         <location line="+16"/>
-        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="-8"/>
-        <location line="+16"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="-4"/>
+        <location line="+14"/>
         <source>Packages</source>
         <translation type="unfinished"></translation>
     </message>
@@ -114,19 +114,19 @@
     </message>
     <message>
         <location line="+4"/>
-        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="+6"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="+4"/>
         <source>FeaturePackage</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="-2"/>
-        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="-4"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="+2"/>
         <source>Feature Package Files</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="+8"/>
-        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="+6"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="+2"/>
         <source>Russian help</source>
         <translation type="unfinished"></translation>
     </message>
@@ -141,14 +141,14 @@
         <location line="+10"/>
         <location line="+24"/>
         <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="+4"/>
-        <location line="+10"/>
-        <location line="+26"/>
+        <location line="+12"/>
+        <location line="+22"/>
         <source>Products</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="-30"/>
-        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="-32"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="-30"/>
         <source>Instances</source>
         <translation type="unfinished"></translation>
     </message>
@@ -167,14 +167,14 @@
     <message>
         <location line="+4"/>
         <location line="+24"/>
-        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="+4"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="+2"/>
         <location line="+22"/>
         <source>Product Licensing</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="-20"/>
-        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="-18"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="-16"/>
         <source>Edit</source>
         <translation type="unfinished"></translation>
     </message>
@@ -190,7 +190,7 @@
         <location line="+4"/>
         <location line="+6"/>
         <location line="+2"/>
-        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="+4"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="+2"/>
         <location line="+6"/>
         <location line="+2"/>
         <source>Product Instance</source>
@@ -198,19 +198,19 @@
     </message>
     <message>
         <location line="-10"/>
-        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="-10"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="-6"/>
         <source>Installations</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="+4"/>
-        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="+4"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="+2"/>
         <source>License file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="+2"/>
-        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="+2"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/LisaLoc/CLisaLoc.cpp" line="+6"/>
         <source>Product Instance File</source>
         <translation type="unfinished"></translation>
     </message>
