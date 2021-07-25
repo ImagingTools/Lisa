67b89d6834dfe3c49ab2216a5b5650af951252a8
kl <kl@21205d9f-8918-2240-a3d5-c39064a2c852>
2021-07-25 17:36:21 +0000

    working on database schema for Lisa
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@3510 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Impl/LisaExe/Resources/Database/Database.sql
@@ -0,0 +1,25 @@
+CREATE TABLE Packages(
+   Id VARCHAR (1000) NOT NULL,
+   Name VARCHAR (1000) NOT NULL,
+   PRIMARY KEY (Id)
+);
+
+
+CREATE TABLE Features(
+  Id VARCHAR (1000) NOT NULL,
+  Name VARCHAR (1000) NOT NULL,
+  PackageId VARCHAR (1000) NOT NULL,
+  PRIMARY KEY (Id),
+  FOREIGN KEY (PackageId) REFERENCES Packages(Id)
+);
+
+
+CREATE TABLE FeatureDependencies(
+  FeatureId VARCHAR (1000) NOT NULL,
+  DependencyId VARCHAR (1000) NOT NULL,
+  PRIMARY KEY (FeatureId, DependencyId),
+  FOREIGN KEY (FeatureId) REFERENCES Features(Id),
+  FOREIGN KEY (DependencyId) REFERENCES Features(Id)  
+);
+
+
