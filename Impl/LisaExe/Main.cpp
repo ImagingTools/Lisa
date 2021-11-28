3235a29751c232a2dd5e51f9b7202c611b4e96f5
kl <kl@21205d9f-8918-2240-a3d5-c39064a2c852>
2021-11-28 11:48:28 +0000

    Production results in the database implemented
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@3841 21205d9f-8918-2240-a3d5-c39064a2c852

--- Impl/LisaExe/Main.cpp
+++ Impl/LisaExe/Main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 {
 	Q_INIT_RESOURCE(imtstyle);
 	Q_INIT_RESOURCE(AcfLoc);
-	Q_INIT_RESOURCE(IacfLoc);
+//	Q_INIT_RESOURCE(IacfLoc);
 	Q_INIT_RESOURCE(AcfSlnLoc);
 	Q_INIT_RESOURCE(ImtCoreLoc);
 	Q_INIT_RESOURCE(LisaLoc);
