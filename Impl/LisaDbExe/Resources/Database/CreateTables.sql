ed3af5554556d4183a6179edd6f85b3fed4a58be
kl <kl@21205d9f-8918-2240-a3d5-c39064a2c852>
2021-12-12 09:35:48 +0000

    Working on Lisa continued. Account page added
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@3877 21205d9f-8918-2240-a3d5-c39064a2c852

--- Impl/LisaDbExe/Resources/Database/CreateTables.sql
+++ Impl/LisaDbExe/Resources/Database/CreateTables.sql
@@ -1,23 +1,48 @@
+
+/**
+	Logical package/container of features.
+*/
 CREATE TABLE Packages(
-   Id VARCHAR (1000) NOT NULL,
-   Name VARCHAR (1000) NOT NULL,
-   Description VARCHAR (1000),
-   Added TIMESTAMP,
-   LastModified TIMESTAMP,
-   PRIMARY KEY (Id)
+	Id VARCHAR (1000) NOT NULL,
+	Name VARCHAR (1000) NOT NULL,
+	Description VARCHAR (1000),
+	Added TIMESTAMP,
+	LastModified TIMESTAMP,
+	PRIMARY KEY (Id)
 );
 
 
+/**
+	Features assigned to defined packages.
+*/
 CREATE TABLE Features(
-  Id VARCHAR (1000) NOT NULL,
-  Name VARCHAR (1000) NOT NULL,
-  Description VARCHAR (1000),
-  PackageId VARCHAR (1000) NOT NULL,
-  PRIMARY KEY (Id, PackageId),
-  FOREIGN KEY (PackageId) REFERENCES Packages(Id) ON DELETE CASCADE ON UPDATE CASCADE
+	Id VARCHAR (1000) NOT NULL,
+	Name VARCHAR (1000) NOT NULL,
+	Description VARCHAR (1000),
+	PackageId VARCHAR (1000) NOT NULL,
+	PRIMARY KEY (Id, PackageId),
+	FOREIGN KEY (PackageId) REFERENCES Packages(Id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
+/**
+	Dependencies between features. A Feature B can technically depend on Feature A (By example Feature <Biometric Access> is based on Feature <User Management>.
+	In this case including the feature B into a product license means that the Feature A is should be also included.
+*/
+CREATE TABLE FeatureDependencies(
+  FeatureId VARCHAR (1000) NOT NULL,
+  FeaturePackageId VARCHAR (1000) NOT NULL,
+  DependencyId VARCHAR (1000) NOT NULL,
+  DependencyPackageId VARCHAR (1000) NOT NULL,
+  PRIMARY KEY (FeatureId, FeaturePackageId, DependencyId, DependencyPackageId),
+  FOREIGN KEY (FeatureId, FeaturePackageId) REFERENCES Features(Id, PackageId),
+  FOREIGN KEY (DependencyId, DependencyPackageId) REFERENCES Features(Id, PackageId)
+);
+
+
+/**
+	List of products.
+*/
 CREATE TABLE Products(
   Id VARCHAR (1000) NOT NULL,
   Name VARCHAR (1000) NOT NULL,
@@ -26,6 +51,9 @@ CREATE TABLE Products(
 );
 
 
+/**
+	List of available product licenses
+*/
 CREATE TABLE ProductLicenses(
   Id VARCHAR (1000) NOT NULL,
   Name VARCHAR (1000) NOT NULL,
@@ -36,6 +64,9 @@ CREATE TABLE ProductLicenses(
 );
 
 
+/**
+	A product license is defined as a set of features
+*/
 CREATE TABLE ProductLicenseFeatures(
   LicenseId VARCHAR (1000) NOT NULL,
   FeatureId VARCHAR (1000) NOT NULL,
@@ -46,15 +77,42 @@ CREATE TABLE ProductLicenseFeatures(
 );
 
 
+CREATE TYPE AccountType AS ENUM ('private', 'company');
 
-CREATE TABLE FeatureDependencies(
-  FeatureId VARCHAR (1000) NOT NULL,
-  FeaturePackageId VARCHAR (1000) NOT NULL,
-  DependencyId VARCHAR (1000) NOT NULL,
-  DependencyPackageId VARCHAR (1000) NOT NULL,
-  PRIMARY KEY (FeatureId, FeaturePackageId, DependencyId, DependencyPackageId),
-  FOREIGN KEY (FeatureId, FeaturePackageId) REFERENCES Features(Id, PackageId),
-  FOREIGN KEY (DependencyId, DependencyPackageId) REFERENCES Features(Id, PackageId)
+CREATE TABLE Accounts(
+  Id VARCHAR (1000) NOT NULL,
+  Name VARCHAR (1000) NOT NULL,
+  Description VARCHAR (1000),
+  Type AccountType NOT NULL,
+  OwnerMail VARCHAR (1000) NOT NULL,
+  OwnerFirstName VARCHAR (1000) NOT NULL,
+  OwnerLastName VARCHAR (1000) NOT NULL,
+  PRIMARY KEY (Id)
+);
+
+
+CREATE TABLE ProductInstances(
+	InstanceId VARCHAR (1000) NOT NULL,
+	ProductId VARCHAR (1000) NOT NULL,
+	AccountId VARCHAR (1000) NOT NULL,
+	Name VARCHAR (1000) NOT NULL,
+	Description VARCHAR (1000),
+	PRIMARY KEY (InstanceId),
+	FOREIGN KEY (AccountId) REFERENCES Accounts(Id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (ProductId) REFERENCES Products(Id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+CREATE TABLE LicenseFiles(
+  Id VARCHAR (1000) NOT NULL,
+  InstanceId VARCHAR (1000) NOT NULL,
+  AccountId VARCHAR (1000) NOT NULL,
+  FileContents VARCHAR (1000) NOT NULL,
+  Description VARCHAR (1000),
+  GeneratedAt TIMESTAMP,
+  PRIMARY KEY (Id),
+  FOREIGN KEY (AccountId) REFERENCES Accounts(Id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (InstanceId) REFERENCES ProductInstances(InstanceId) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
