81a7abaebe4e948f42968778a34d07bc26f2bf86
Victor.Shcherbina <Victor.Shcherbina@21205d9f-8918-2240-a3d5-c39064a2c852>
2023-04-03 08:17:24 +0000

    LisaServer for web corrected
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@6234 21205d9f-8918-2240-a3d5-c39064a2c852

--- Impl/LisaServer/Main.cpp
+++ Impl/LisaServer/Main.cpp
@@ -11,7 +11,9 @@
 
 int main(int argc, char *argv[])
 {
-//	Q_INIT_RESOURCE(lisaqmlWeb);
+#ifdef WEB_COMPILE
+	Q_INIT_RESOURCE(lisaqmlWeb);
+#endif
 	Q_INIT_RESOURCE(lisaqml);
 	Q_INIT_RESOURCE(ImtCoreLoc);
 	Q_INIT_RESOURCE(LisaLoc);
