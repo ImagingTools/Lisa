1670b3e0e1b66c8fba7ff96acd67d7199eacf148
Roman.Kucherenko <Roman.Kucherenko@21205d9f-8918-2240-a3d5-c39064a2c852>
2023-04-18 04:39:07 +0000

    Lisa migrations corrected
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@6334 21205d9f-8918-2240-a3d5-c39064a2c852

--- Build/Migrations/migration_9.sql
+++ Build/Migrations/migration_9.sql
@@ -1,24 +1,3 @@
-DELETE FROM public."GroupUsers";
-DROP TABLE public."GroupUsers";
-
-DELETE FROM public."ParentGroups";
-DROP TABLE public."ParentGroups";
-
-DELETE FROM public."UserGroups";
-DROP TABLE public."UserGroups";
-
-CREATE TABLE public."UserGroups"
-(
-    "Id" SERIAL,
-    "DocumentId" character varying(1000) COLLATE pg_catalog."default" NOT NULL,
-    "Document" jsonb,
-    "RevisionNumber" bigint,
-    "LastModified" timestamp without time zone,
-    "Checksum" bigint,
-    "IsActive" boolean,
-    CONSTRAINT "Orders_pkey" PRIMARY KEY ("Id")
-);
-
 DROP TABLE "Roles" CASCADE;
 DROP TABLE "Users" CASCADE;
 
