f25100a56121cfcd4f4198fd1fdb6efa638ec153
Victor.Shcherbina <Victor.Shcherbina@21205d9f-8918-2240-a3d5-c39064a2c852>
2023-07-14 00:31:50 +0000

    imtstyleqml added
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@7056 21205d9f-8918-2240-a3d5-c39064a2c852

--- Impl/LisaQmlExe/Main.cpp
+++ Impl/LisaQmlExe/Main.cpp
@@ -25,6 +25,7 @@ int main(int argc, char *argv[])
 	Q_INIT_RESOURCE(imtguigql);
 	Q_INIT_RESOURCE(imtqml);
 	Q_INIT_RESOURCE(imtstyle);
+	Q_INIT_RESOURCE(imtstyleqml);
 	Q_INIT_RESOURCE(imtgui);
 	Q_INIT_RESOURCE(lisaqml);
 	Q_INIT_RESOURCE(ImtCoreLoc);
