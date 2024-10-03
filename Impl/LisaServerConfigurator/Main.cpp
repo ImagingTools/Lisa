2521acb33a0b8154a3c1507683e7004c369076cf
Roman.Kucherenko <Roman.Kucherenko@21205d9f-8918-2240-a3d5-c39064a2c852>
2024-10-03 04:12:35 +0000

    Code cleanup; Partitura corrected;
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@11314 21205d9f-8918-2240-a3d5-c39064a2c852

--- Impl/LisaServerConfigurator/Main.cpp
+++ Impl/LisaServerConfigurator/Main.cpp
@@ -10,9 +10,7 @@ int main(int argc, char *argv[])
 {
 	Q_INIT_RESOURCE(LisaServerConfigurator);
 	Q_INIT_RESOURCE(lisaqml);
-
 	qmlRegisterType<imtqml::CQmlProcess>("imtqml", 1, 0, "Process");
-
 	return Run<CLisaServerConfigurator, DefaultImtCoreQmlInitializer>(argc, argv);
 }
 
