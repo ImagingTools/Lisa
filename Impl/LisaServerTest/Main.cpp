2e20aa4a341d07e02e4d4de2329e29a2e0a2b4e4
Roman Kucherenko <kucherenkord@gmail.com>
2026-07-06 10:47:25 +0600

    LisaServerTest added

---
+++ Impl/LisaServerTest/Main.cpp
@@ -0,0 +1,21 @@
+// ImtCore includes
+#include <imtlic/Init.h>
+
+// Lisa includes
+#include <GeneratedFiles/LisaServerTest/CLisaServerTest.h>
+#include "LisaFeatures.h"
+
+
+int main(int argc, char *argv[])
+{
+#ifdef WEB_COMPILE
+	Q_INIT_RESOURCE(lisaqmlWeb);
+#endif
+	Q_INIT_RESOURCE(lisaqml);
+	Q_INIT_RESOURCE(LisaLoc);
+	Q_INIT_RESOURCE(imtlicguiTheme);
+
+	return ProductFeatureRun<CLisaServerTest, DefaultImtCoreQmlInitializer, lisa::FillProduct>(argc, argv);
+}
+
+
