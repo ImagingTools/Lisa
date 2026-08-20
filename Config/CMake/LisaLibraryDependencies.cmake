688b976e7d60f9ea3faae5588e8b6d17ab0bcb24
Sergey Zastrovnyh <sergeizastrovnyh@yandex.com>
2026-08-20 13:48:04 +0600

    Refactor CMake configuration to replace custom library dependency declaration function with modern acf_declare_target_dependencies for improved consistency and maintainability

--- Config/CMake/LisaLibraryDependencies.cmake
+++ Config/CMake/LisaLibraryDependencies.cmake
@@ -26,43 +26,13 @@
 # targets have been created.
 # ---------------------------------------------------------------------------
 
-# Declare the dependencies of a Lisa library, ignoring any entry whose target
-# does not exist in the current configuration (for example feature-gated
-# libraries, or ImtCore::/Acf::/AcfSln::/IAcf:: targets that are not available).
-function(lisa_declare_library_dependencies target)
-	cmake_parse_arguments(ARG "" "LINK_SCOPE" "" ${ARGN})
-
-	if(NOT ARG_LINK_SCOPE)
-		set(ARG_LINK_SCOPE ${ACF_LIBRARY_LINK_SCOPE})
-	endif()
-
-	if(NOT TARGET ${target})
-		return()
-	endif()
-
-	# target_link_libraries() is illegal on an ALIAS target (e.g. in-tree ImtCore::
-	# aliases in unified builds), and augmenting the real target can inject dependency
-	# cycles through the Qt autogen targets. Never target an alias.
-	get_target_property(_lisa_aliased ${target} ALIASED_TARGET)
-	if(_lisa_aliased)
-		return()
-	endif()
-
-	foreach(dependency IN LISTS ARG_UNPARSED_ARGUMENTS)
-		if(TARGET ${dependency})
-			target_link_libraries(${target} ${ARG_LINK_SCOPE} ${dependency})
-		endif()
-	endforeach()
-endfunction()
-
-
 # --- Libraries --------------------------------------------------------------
-lisa_declare_library_dependencies(lisadb		LINK_SCOPE PUBLIC	ImtCore::imtlic)
+acf_declare_target_dependencies(lisadb		LINK_SCOPE PUBLIC	ImtCore::imtlic)
 
 # --- QML web-resource libraries ---------------------------------------------
 if(QT_VERSION_MAJOR EQUAL 6)
-	lisa_declare_library_dependencies(lisaqml	LINK_SCOPE PUBLIC	Qt${QT_VERSION_MAJOR}::Core5Compat)
+	acf_declare_target_dependencies(lisaqml	LINK_SCOPE PUBLIC	Qt${QT_VERSION_MAJOR}::Core5Compat)
 endif()
 
 # --- Arxc-generated static libraries ----------------------------------------
-lisa_declare_library_dependencies(LisaLoc		LINK_SCOPE PUBLIC	Acf::icomp)
+acf_declare_target_dependencies(LisaLoc		LINK_SCOPE PUBLIC	Acf::icomp)
